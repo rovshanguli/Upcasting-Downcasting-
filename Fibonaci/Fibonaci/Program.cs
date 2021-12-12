57fedd30b2ff4032e25d9762a02cc5b061d7a5d5
rovshanguli <rovshanshg@code.edu.az>
2021-12-12 19:38:19 +0400

    Fibonaci

---
+++ Fibonaci/Fibonaci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstNumbuer = 0;
+            int SecondNumber = 1;
+            int count = 0;
+            for(int i = 2; i< 15; i++)
+            {
+                count = FirstNumbuer + SecondNumber;
+                FirstNumbuer = SecondNumber;
+                SecondNumber = count;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
